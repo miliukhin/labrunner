8be74967750433eb4f93c56786de3e6a49b8f0a5
Oleksandr Miliukhin <soapmintus@gmail.com>
2024-03-14 20:00:11 +0200

    норм версія

--- main.go
+++ main.go
@@ -50,48 +50,25 @@ func RunTasks(page *rod.Page) error {
 			// log.Print(err)
 			fmt.Printf("%s\n", err)
 		} else {
-	        	element.MustElement(".form-control").MustInput(string(result))
-		}
-	}
-	return nil
-}
-
-// тре субміт тільки тим елементам, де нема Правильно/неправильно + по одному
-// так не паше, тоді списку не треба, просто перший без обирається
-// але будуть нюанси з початковими тестами? ні, якщо неправильно теж вважати то норм
-// кароче не хоче вміти
-func SubmitAnswersInRust (page *rod.Page) error {
-	elements := page.MustElements(".formulation.clearfix")
-	for _, element := range elements {
-		// badge := element.MustElementR("div", "равильно")
-		badge := element.MustElementX("//div[contains@class='badge']")
-		// fmt.Println(badge.MustText())
-		if badge == nil {
-			fmt.Println("НЄ бейдж")
-		} else {
-			fmt.Println("Є")
-			// button := element.MustElement("input[type='submit'][value='Перевірити']")
-			// if button != nil {
-			// 	fmt.Println(button.MustText())
-			// 	button.MustClick()
-			// } else {
-			// }
-		}
-	}
-// <div class="correctness badge correct">Правильно</div>
-// #yui_3_18_1_1_1710351328609_408 > div:nth-child(1)
-// /html/body/div[1]/div[2]/div/div[1]/section/div[2]/form/div/div[6]/div[2]/div[2]/div/div[1]
-	return nil
-}
-
-// це стара теж погана
-func SubmitAnswers (page *rod.Page) error {
-	buttons := page.MustElements("input[type='submit'][value='Перевірити']")
-	for _, button := range buttons {
-		if button != nil {
-			button.MustClick()
-		} else {
-			fmt.Println("Button not found")
+			form, formerr := element.Element(".form-control")
+			radioBoxes, raderr := element.Elements("input[type='radio']")
+			if formerr == nil {
+	        		form.MustInput(string(result))
+			} else if raderr == nil {
+				for len(result) > 0 && (result[len(result)-1] == '\n' || result[len(result)-1] == '\r') {
+        				result = result[:len(result)-1]
+    				}
+				// fmt.Println("solving radio:")
+				answers := element.MustElements(".flex-fill.ml-1")
+				for i, a := range answers {
+					// fmt.Println(string(result))
+					// fmt.Println(a.MustText())
+					if a.MustText() == string(result) {
+						radioBoxes[i].MustClick()
+						break
+					}
+				}
+			}
 		}
 	}
 	return nil
@@ -99,27 +76,24 @@ func SubmitAnswers (page *rod.Page) error {
 
 func main() {
 
-	if len(os.Args) < 3 {
-		fmt.Println("name <email> <password>")
+	if len(os.Args) < 4 {
+		fmt.Println("name <link> <email> <password>")
 		return
 	}
-	email := os.Args[1]
-	pass := os.Args[2]
+	link	:= os.Args[1]
+	email	:= os.Args[2]
+	pass	:= os.Args[3]
 
-	page := rod.New().NoDefaultDevice().MustConnect().MustPage("https://vns.lpnu.ua/mod/quiz/view.php?id=835577")
+	page := rod.New().NoDefaultDevice().MustConnect().MustPage(link)
 	page.MustElement("#region-main > div > div > div > div > div.login-identityproviders > a").MustClick()
 	page.MustElement("#identifierId").MustInput(email)
 	page.MustElement("#identifierNext > div > button").MustClick()
 	page.MustElement("#password").MustInput(pass)
 	page.MustElement("#passwordNext > div > button").MustClick()
 
-	// StartNextAttempt(page)
-	// SubmitAnswersInRust(page)
-	// SubmitAnswers(page)
-
 	err := StartNextAttempt(page)
 	if err != nil {
-		log.Fatalln("Error while startin new attempt:", err)
+		log.Fatalln("Error while starting new attempt:", err)
 	}
 	isLastPage := false
 	for !isLastPage {
